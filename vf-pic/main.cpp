e1b6f5b125a6cf22f688c9639eabb7faba149c57
Tobias Heinemann <tobi@zwicky.berkeley.edu>
2013-01-27 15:53:14 -0800

    Initial Commit

---
+++ vf-pic/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  vf-pic
+//
+//  Created by Tobias Heinemann on 1/27/13.
+//  Copyright (c) 2013 Tobias Heinemann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
