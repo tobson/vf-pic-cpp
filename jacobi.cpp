85c5f57180d6df90ff0a002f2d85b30c52f43c70
Tobias Heinemann <tobi@Tobiass-MacBook-Air.local>
2013-01-27 22:31:34 -0800

    Added new executable "jacobi".

---
+++ jacobi.cpp
@@ -0,0 +1,47 @@
+//
+//  test.cpp
+//  vf-pic
+//
+//  Created by Tobias Heinemann on 1/27/13.
+//  Copyright (c) 2013 Tobias Heinemann. All rights reserved.
+//
+
+#include "config.h"
+#include "global.h"
+#include "scalar-field.h"
+#include "vector-field.h"
+
+#include <iostream>
+#include <libconfig.h++>
+#include <vector>
+
+int main(int argc, const char * argv[])
+{
+  std::string srcdir = ".";
+  if (argc > 1) srcdir = argv[1];
+  
+  try
+  {
+    config::read (srcdir + "/config.in");
+    global::computeVariables();
+    config::write (srcdir + "/config.out");
+  }
+  catch (const libconfig::ConfigException& e)
+  {
+    std::cout << e.what () << std::endl;
+    return 1;
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what () << std::endl;
+    return 1;
+  }
+  catch (...)
+  {
+    std::cout << "Unknown exception" << std::endl;
+    return 1;
+  }
+  
+  std::cout << "Hello, World! My name is " << argv[0] << ".\n";
+  return 0;
+}
