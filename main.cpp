1e21fd242c8d31514d7dd676a5998f05596f6794
Tobias Heinemann <tobias.heinemann@gmail.com>
2013-01-27 16:12:41 -0800

    Move main source file to top level. Not quite sure how this works yet...

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  test.cpp
+//  vf-pic
+//
+//  Created by Tobias Heinemann on 1/27/13.
+//  Copyright (c) 2013 Tobias Heinemann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+  
+  // insert code here...
+  std::cout << "Hello, World! My name is " << argv[0] << ".\n";
+  return 0;
+}
