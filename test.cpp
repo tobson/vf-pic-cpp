a3d22acd339f66baf84e873c042c5edfb9fbf1f0
Tobias Heinemann <tobias.heinemann@gmail.com>
2013-01-27 17:01:14 -0800

    Added new executable "test" for testing purposes.

---
+++ test.cpp
@@ -0,0 +1,39 @@
+//
+//  test.cpp
+//  vf-pic
+//
+//  Created by Tobias Heinemann on 1/27/13.
+//  Copyright (c) 2013 Tobias Heinemann. All rights reserved.
+//
+
+#include "config.h"
+#include "global.h"
+
+#include <iostream>
+#include <libconfig.h++>
+
+int main(int argc, const char * argv[])
+{
+  std::string srcdir = ".";
+  if (argc > 1) srcdir = argv[1];
+  
+  try
+  {
+    config::read (srcdir + "/config.in");
+    global::computeVariables();
+    config::write (srcdir + "/config.out");
+  }
+  catch (const libconfig::ConfigException& e)
+  {
+    return 1;
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what () << std::endl;
+    return 1;
+  }
+  
+  // insert code here...
+  std::cout << "Hello, World! My name is " << argv[0] << ".\n";
+  return 0;
+}
