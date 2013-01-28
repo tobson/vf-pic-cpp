dea6227f458e458f1a7a0d9ee30a7a58b9e72068
Tobias Heinemann <tobias.heinemann@gmail.com>
2013-01-27 17:30:15 -0800

    Added barrier class.

---
+++ barrier.h
@@ -0,0 +1,25 @@
+//
+//  barrier.h
+//  vf-pic
+//
+//  Created by Tobias Heinemann on 1/27/13.
+//  Copyright (c) 2013 Tobias Heinemann. All rights reserved.
+//
+
+#ifndef __vf_pic__barrier__
+#define __vf_pic__barrier__
+
+#include <atomic>
+
+class Barrier
+{
+public:
+  Barrier (const int);
+  bool wait ();
+private:
+  const int nthreads;
+  std::atomic<int> count;
+  std::atomic<bool> generation;
+};
+
+#endif /* defined(__vf_pic__barrier__) */
