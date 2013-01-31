062f09476006541db05efa0ff8cc8e7ab0847af9
Tobias Heinemann <tobias.heinemann@gmail.com>
2013-01-30 17:37:24 -0800

    Allow the two arguments A and E of faraday (A, E) to be vector fields of different scalar field type.

--- faraday.h
+++ faraday.h
@@ -12,17 +12,20 @@
 #include "scalar-field.h"
 #include "three-vector.h"
 
-template <class S>
-void faraday (ThreeVector<S>& A, const ThreeVector<S>& E)
+template <class S, class T>
+void faraday (ThreeVector<S>& A, const ThreeVector<T>& E)
 {
-    static_assert (is_scalarfield<S>::value, "Class must be of ScalarField type");
+    static_assert (is_scalarfield<S>::value, "Class S must be of ScalarField type");
+    static_assert (is_scalarfield<T>::value, "Class T must be of ScalarField type");
+    
+    static_assert (S::nx == T::nx && S::nz == T::nz, "Field dimensions don't match");
 
     const real dt = config::dt;
     
     for (int j = 0; j < 3; ++j)
     {
-        for (int k = 1; k <= S::nz; ++k)
-        for (int i = 1; i <= S::nx; ++i)
+        for (int k = 1; k <= T::nz; ++k)
+        for (int i = 1; i <= T::nx; ++i)
         {
             A[j](k,i) -= dt*E[j](k,i);
         }
