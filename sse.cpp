6776e095d5fed3ef31817c9796421196a6742c92
Tobias Heinemann <tobias.heinemann@gmail.com>
2013-02-02 17:23:11 -0800

    Added new target for testing SSE instructions.

---
+++ sse.cpp
@@ -0,0 +1,14 @@
+//
+//  sse.cpp
+//  vf-pic
+//
+//  Created by Tobias Heinemann on 2/1/13.
+//  Copyright (c) 2013 Tobias Heinemann. All rights reserved.
+//
+
+#include <xmmintrin.h>
+
+int main (int argc, const char * argv[])
+{
+    return 0;
+}
