8b221a3fc8dc893fb9a2dd4e4ae5739f920ffe9d
Tobias Heinemann <tobias.heinemann@gmail.com>
2013-02-11 22:37:37 -0800

    Cosmetics.

--- grid.cpp
+++ grid.cpp
@@ -11,16 +11,20 @@
 
 Grid::Grid ()
 {
-    for (int k = 0; k < vfpic::mz + 2; ++k)
-    for (int i = 0; i < vfpic::mx + 2; ++i)
+    using namespace config;
+    using namespace vfpic;
+    
+    for (int k = 0; k < mz + 2; ++k)
+    for (int i = 0; i < mx + 2; ++i)
     {
-        x(k,i) = config::x0 + (real (i) - real (0.5))*vfpic::dx;
-        z(k,i) = config::z0 + (real (k) - real (0.5))*vfpic::dz;
+        x(k,i) = x0 + (real (i) - 0.5)*dx;
+        z(k,i) = z0 + (real (k) - 0.5)*dz;
     }
 }
 
 std::ostream& operator<< (std::ostream& os, const Grid& grid)
 {
-    os << grid.x << grid.z;
+    os << grid.x;
+    os << grid.z;
     return os;
 }
