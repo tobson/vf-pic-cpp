73b63ea25d63c9040a2f7fa0de33113d2376b24b
Tobias Heinemann <tobias.heinemann@gmail.com>
2013-02-15 19:33:46 -0800

    Made python module functional.

--- vfpic.py
+++ vfpic.py
@@ -2,7 +2,7 @@
 
 from numpy import ndarray
 
-class config:
+class Config:
 
     def __init__ (self, filename = "config.out"):
 
@@ -25,61 +25,84 @@ class config:
 
         f.close ()
 
+class Particles (ndarray):
+
+    def __new__ (cls, config = None):
+        from numpy import float32
+        if not isinstance (config, Config): config = Config ()
+        dtype = [( 'x',float32),( 'z',float32),
+                 ('vx',float32),('vy',float32),('vz',float32)]
+        obj = ndarray.__new__ (cls, shape = (config.npar,), dtype = dtype)
+        return obj
+  
+    def __array_finalize__ (self, obj):
+        if obj is None: return
+  
+    def read (self, f):
+        from numpy import fromfile
+        self[...] = fromfile (f, self.dtype, self.size)
+        return self.size*self.itemsize
+
+class ScalarField (ndarray):
+
+    def __new__ (cls, config = None):
+        from numpy import float32
+        if not isinstance (config, Config): config = Config ()
+        dtype = float32
+        shape = (config.nz + 2, config.nx + 2)
+        obj = ndarray.__new__ (cls, shape = shape, dtype = dtype)
+        return obj
+
+    def __array_finalize__ (self, obj):
+        if obj is None: return
+
+    def read (self, f):
+        from numpy import fromfile
+        self[...] = fromfile (f, self.dtype, self.size).reshape (self.shape)
+        return self.size*self.itemsize
+
+    def trim (self):
+        return self[1:-1,1:-1]
+
 class State:
 
     def __init__ (self, config = None):
 
-        from numpy import ndarray, float64
+        from numpy import ndarray, float32
 
         if not isinstance (config, Config): config = Config ()
 
-        self.lwrite_sources = config.lwrite_sources
+        self.Ax = ScalarField (config)
+        self.Ay = ScalarField (config)
+        self.Az = ScalarField (config)
 
-        self.particles = Particles (config = config)
+        self.Bx = ScalarField (config)
+        self.By = ScalarField (config)
+        self.Bz = ScalarField (config)
 
-        if self.lwrite_sources:
+        self.Ex = ScalarField (config)
+        self.Ey = ScalarField (config)
+        self.Ez = ScalarField (config)
 
-            self.rho = ScalarField ()
+        self.particles = Particles (config)
 
-            self.rux = ScalarField ()
-            self.ruy = ScalarField ()
-            self.ruz = ScalarField ()
+        self.rho = ScalarField (config)
 
-        self.Ax = ScalarField ()
-        self.Ay = ScalarField ()
-        self.Az = ScalarField ()
-
-        self.Bx = ScalarField ()
-        self.By = ScalarField ()
-        self.Bz = ScalarField ()
-
-        self.Ex = ScalarField ()
-        self.Ey = ScalarField ()
-        self.Ez = ScalarField ()
+        self.rux = ScalarField (config)
+        self.ruy = ScalarField (config)
+        self.ruz = ScalarField (config)
         
-        self.x = ndarray ((config.mx,), dtype = float64)
-        self.z = ndarray ((config.mz,), dtype = float64)
+        self.x = ScalarField (config)
+        self.z = ScalarField (config)
         
         self.it = 0
-        self.t = 0.0
+        #self.t = 0.0
 
     def read (self, f):
 
-        from numpy import fromfile, int64, float64
+        from numpy import fromfile, int64, float32
 
         recordsize = 0
-
-        # Read particles
-        recordsize += self.particles.read (f)
-
-        # Read sources
-        if self.lwrite_sources:
-
-            recordsize += self.rho.read (f)
-
-            recordsize += self.rux.read (f)
-            recordsize += self.ruy.read (f)
-            recordsize += self.ruz.read (f)
 
         # Read fields
         recordsize += self.Ax.read (f)
@@ -93,17 +116,54 @@ class State:
         recordsize += self.Ex.read (f)
         recordsize += self.Ey.read (f)
         recordsize += self.Ez.read (f)
+
+        # Read particles
+        recordsize += self.particles.read (f)
+
+        # Read sources
+        recordsize += self.rho.read (f)
+
+        recordsize += self.rux.read (f)
+        recordsize += self.ruy.read (f)
+        recordsize += self.ruz.read (f)
         
         # Read grid
-        self.x[...] = fromfile (f, dtype = float64, count = self.x.size)
-        self.z[...] = fromfile (f, dtype = float64, count = self.z.size)
-        
-        # Read time step
+        recordsize += self.x.read (f)
+        recordsize += self.z.read (f)
+
+        # Read time
         self.it, = fromfile (f, dtype = int64, count = 1)
         recordsize += self.it.nbytes
         
-        # Read time
-        self.t, = fromfile (f, dtype = float64, count = 1)
-        recordsize += self.t.nbytes
-        
         return recordsize
+
+class DataFile ():
+
+    def __init__ (self, config = None, filename = "data/var.dat"):
+
+        if not isinstance (config, Config): config = Config ()
+
+        self.f = open (filename)
+
+        # Determine end of file
+        self.f.seek (0, 2)
+        self.eof = self.f.tell ()
+        self.f.seek (0)
+
+        self.state = State (config)
+
+        self.recordsize = 0
+        print "eof = ", self.eof
+
+    def __iter__ (self):
+
+        while self.eof - self.f.tell () >= self.recordsize:
+
+            self.recordsize = self.state.read (self.f)
+            print "recordsize = ", self.recordsize
+
+            yield self.state
+
+    def __del__ (self):
+
+        self.f.close ()
