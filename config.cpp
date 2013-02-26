a8a5fbcf64e9ab5f71742e7170267cfe12a0c88c
Tobias Heinemann <tobias.heinemann@gmail.com>
2013-02-26 14:08:29 -0800

    Removed cruft.

--- config.cpp
+++ config.cpp
@@ -11,8 +11,7 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <libconfig.h++>
-#include <yaml-cpp/yaml.h>
+#include <sstream>
 
 #include "global.h"
 
@@ -115,50 +114,6 @@ namespace config
     bool verbose = false;
     bool throwOnCopyConstruct = false;
 
-    struct ReadVariable
-    {
-        ReadVariable (const std::string& srcdir):
-        config (YAML::LoadFile (srcdir + "/input.yaml")),
-        output (srcdir + "/output.yaml")
-        {
-            /* Store precision */
-            config["precision"] = sizeof (real);
-
-            /* Append parameters */
-            config["nx"] = vfpic::nx;
-            config["nz"] = vfpic::nz;
-            config["npc"] = vfpic::npc;
-            config["nthreads"] = vfpic::nthreads;
-            config["alignment"] = vfpic::alignment;
-
-            /* Also append global constants */
-            config["mx"] =  vfpic::mx;
-            config["mz"] = vfpic::mz;
-            config["npar"] = vfpic::npar;
-            config["mpar"] = vfpic::mpar;
-        }
-        template <typename T>
-        void operator() (const std::string& name, T& value)
-        {
-            value = config[name].as<T> (value);
-            config[name] = value;
-        }
-        ~ReadVariable ()
-        {
-            output << YAML::Dump (config) << std::endl;
-            output.close ();
-        }
-        YAML::Node config;
-        std::ofstream output;
-    };
-    
-    template <typename T>
-    void readVariable (YAML::Node& node, const std::string& name, T& value)
-    {
-        value = node[name].as<T> (value);
-        node[name] = value;
-    }
-
     void Config::parse (const char *filename)
     {
         std::ifstream file (filename);
@@ -218,10 +173,7 @@ namespace config
 
                     contents[section][key] = value;
                 }
-                else
-                {
-                    throw NoAssignment (lineNumber);
-                }
+                else throw NoAssignment (lineNumber);
             }
         }
     }
@@ -434,58 +386,5 @@ namespace config
 
         filename = srcdir + "/output.cfg";
         cfg.save (filename.c_str ());
-
-        ReadVariable readVariable (srcdir);
-
-        /* Grid */
-        readVariable ("x0", x0);
-        readVariable ("z0", z0);
-
-        readVariable ("Lx", Lx);
-        readVariable ("Lz", Lz);
-
-        /* Time stepping */
-        readVariable ("dt", dt);
-        readVariable ("nt", nt);
-
-        /* Physics */
-        readVariable ("rho0", rho0);
-        readVariable ("cs0", cs0);
-        readVariable ("em", em);
-
-        /* Shearing sheet */
-        readVariable ("Omega", Omega);
-        readVariable ("qshear", qshear);
-
-        /* Initial condition */
-        readVariable ("ikx", ikx);
-        readVariable ("ikz", ikz);
-        readVariable ("B0", B0);
-        readVariable ("brms", brms);
-        readVariable ("ampl", ampl);
-        readVariable ("randomizeSeed", randomizeSeed);
-        readVariable ("randomizePositions", randomizePositions);
-        readVariable ("initcond", initcond);
-
-        /* Diagnostics and data I/O */
-        readVariable ("itWrite", itWrite);
-        readVariable ("itPrint", itPrint);
-
-        readVariable ("writeSources", writeSources);
-        readVariable ("writeData", writeData);
-        readVariable ("writeDiagnostics", writeDiagnostics);
-        readVariable ("writeParticles", writeParticles);
-
-        readVariable ("overwriteSnapshots", overwriteSnapshots);
-
-        /* Smoothing */
-        readVariable ("filterDensity", filterDensity);
-        readVariable ("filterVelocity", filterVelocity);
-        readVariable ("filterEfield", filterEfield);
-        readVariable ("filterBfield", filterBfield);
-
-        /* Debugging */
-        readVariable ("verbose", verbose);
-        readVariable ("throwOnCopyConstruct", throwOnCopyConstruct);
     }
 }
