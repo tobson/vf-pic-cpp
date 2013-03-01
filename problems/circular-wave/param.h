8e440d2e8b4788aabfc4a34f420ff5525d26bfae
Tobias Heinemann <tobias.heinemann@gmail.com>
2013-02-28 19:15:59 -0800

    More restructuring.

---
+++ problems/circular-wave/param.h
@@ -0,0 +1,26 @@
+//
+//  param.h
+//  vf-pic
+//
+//  Created by Tobias Heinemann on 2/28/13.
+//  Copyright (c) 2013 Tobias Heinemann. All rights reserved.
+//
+
+#ifndef vf_pic_param_h
+#define vf_pic_param_h
+
+typedef double real;
+
+namespace vfpic
+{
+const int nx = 128;
+const int nz = 128;
+
+const int npc = 256;
+
+const int nthreads = 4;
+
+const int alignment = 16;
+}
+
+#endif
