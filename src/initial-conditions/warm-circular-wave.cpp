70e5ebcc8f958eecdefc5a2c84505f05ea944d0b
Tobias Heinemann <tobias.heinemann@gmail.com>
2013-03-09 22:52:42 -0800

    Initialize standing wave.

--- src/initial-conditions/warm-circular-wave.cpp
+++ src/initial-conditions/warm-circular-wave.cpp
@@ -19,33 +19,44 @@
 
 typedef std::complex<real> complex;
 
+const complex I = complex (0,1);
+
+inline complex Z (const complex zeta)
+{
+    return complex (0,1)*sqrt (pi)*Faddeeva::w (zeta);
+}
+
+template <typename T>
+T sqr (const T& x)
+{
+    return pow (x, 2);
+}
+
 complex newton (const real eps = 1e-12)
 {
     using namespace config;
 
-    const real vA2 = B0*B0/rho0;
+    const real vA = sqrt (B0*B0/rho0);
 
     const real kx = 2.0*pi*real (ikx)/Lx;
     const real kz = 2.0*pi*real (ikz)/Lz;
 
-    const real k2 = kx*kx + kz*kz;
+    const real k = sqrt (kx*kx + kz*kz);
 
-    const real kvA = sqrt (k2*vA2);
-    const real kvt = sqrt (k2)*cs0;
+    const real kvA = k*vA;
+    const real kvt = k*cs0;
     const real emB = em*B0;
 
-    const complex I = complex (0,1);
-    auto Z = [] (complex zeta) { return complex (0,1)*sqrt (pi)*Faddeeva::w (zeta); };
-
-    complex omega = kvA + kvt*kvt/(2.0*emB) - I*sqrt (0.125*pi)*(emB*emB/kvt)*exp (-0.5*emB*emB/(kvt*kvt));
+    complex omega = kvA + kvt*kvt/(2.0*emB) - I*sqrt (0.125*pi)*(emB*emB/kvt)*exp (-0.5*sqr(emB/kvt));
 
     for (;;)
     {
         const complex zeta0 = sqrt (0.5)*omega/kvt;
         const complex zeta1 = sqrt (0.5)*(omega + emB)/kvt;
+        const complex Z1 = Z (zeta1);
 
-        const complex f = emB*(emB*zeta0*Z (zeta1) + omega) - kvA*kvA;
-        const complex df = emB*(1.0 - omega*emB/(kvt*kvt) - (emB/omega)*zeta0*(2.0*zeta0*zeta1 - 1.0)*Z (zeta1));
+        const complex f = emB*(emB*zeta0*Z1 + omega) - kvA*kvA;
+        const complex df = emB*(1.0 - omega*emB/(kvt*kvt) - (emB/omega)*zeta0*(2.0*zeta0*zeta1 - 1.0)*Z1);
 
         const complex delta = f/df;
         omega -= delta;
@@ -71,53 +82,53 @@ void initialCondition (GlobalVariables *global)
 
     BoundaryConditions boundCond;
     
-    const real vA2 = B0*B0/rho0;
+    const real vA = sqrt (B0*B0/rho0);
 
     const real kx = 2.0*pi*real (ikx)/Lx;
     const real kz = 2.0*pi*real (ikz)/Lz;
 
-    const real k2 = kx*kx + kz*kz;
+    const real k = sqrt (kx*kx + kz*kz);
+
+    const real kvA = k*vA;
+    const real kvt = k*cs0;
+    const real emB = em*B0;
 
     const real angle = atan2 (kx, kz);
 
-    const real emB = em*B0;
-
-    const real kvA = sqrt (k2*vA2);
-    const real kvt = sqrt (k2)*cs0;
-
     const real eps = kvt/emB;
-    const real beta = 2.0*cs0*cs0/vA2;
+    const real beta = 2.0*sqr (cs0/vA);
 
     std::cout << "eps = " << eps << std::endl;
     std::cout << "beta = " << beta << std::endl;
 
-    // Imaginary unit
-    const complex I (0,1);
-    
-    // Plasma dispersion function
-    auto Z = [] (complex zeta) { return complex (0,1)*sqrt (pi)*Faddeeva::w (zeta); };
-
     const complex omega = newton ();
+
+    const real gamma = std::imag (omega);
 
     const complex zeta0 = sqrt (0.5)*omega/kvt;
     const complex zeta1 = sqrt (0.5)*(omega + emB)/kvt;
 
+    std::cout << "This should be zero: " << emB*(emB*zeta0*Z (zeta1) + omega) - kvA*kvA << std::endl;
+
     const real period = 2.0*pi/std::real (omega);
     std::cout << "Time steps per period: " << period/dt << std::endl;
-    std::cout << "Relative damping rate: " << std::imag (omega)/std::real (omega) << std::endl;
-    std::cout << "Damping per period: " << 1.0 - exp (std::imag (omega)*period) << std::endl;
-    std::cout << emB*(emB*zeta0*Z (zeta1) + omega) - kvA*kvA << std::endl;
+    std::cout << "Relative damping rate: " << gamma/std::real (omega) << std::endl;
+    std::cout << "Damping per period: " << 1.0 - exp (gamma*period) << std::endl;
 
     Vector<complex> Evec, Avec, Uvec;
 
-    Evec.x = +ampl*cos (angle);
-    Evec.y = +ampl*I;
+    Evec.x =  ampl*cos (angle);
+    Evec.y =  ampl*I;
     Evec.z = -ampl*sin (angle);
 
-//    std::cout << Evec.x << ", " << Evec.y << ", " << Evec.z << std::endl;
-
     for (int j = 0; j < 3; ++j) Avec[j] = Evec[j]/(I*omega);
-    for (int j = 0; j < 3; ++j) Uvec[j] = em*zeta0*Z (zeta1)*Avec[j];
+    for (int j = 0; j < 3; ++j) Uvec[j] = Avec[j]*em*zeta0*Z (zeta1);
+
+    for (int j = 0; j < 3; ++j)
+    {
+        Avec[j] *= exp(0.5*I*omega*dt);
+        Uvec[j] *= exp(0.5*I*omega*dt);
+    }
 
     // Randomize particle positions
     for (Particle *p = particles.begin (); p != particles.end (); ++p)
@@ -137,11 +148,11 @@ void initialCondition (GlobalVariables *global)
     // Initialize wave in bulk velocity by shifting the Maxwellian
     for (auto p = particles.begin (); p != particles.end (); ++p)
     {
-        const complex phi = kx*p->x + kz*p->z + 0.5*omega*dt;
+        const real phi = kx*p->x + kz*p->z;
 
-        p->vx += std::real (Uvec.x*exp (I*phi));
-        p->vy += std::real (Uvec.y*exp (I*phi));
-        p->vz += std::real (Uvec.z*exp (I*phi));
+        p->vx += cos (phi)*std::real (Uvec.x);
+        p->vy += cos (phi)*std::real (Uvec.y);
+        p->vz += cos (phi)*std::real (Uvec.z);
     }
     
     removeMeanMomentum (particles);
@@ -149,21 +160,14 @@ void initialCondition (GlobalVariables *global)
     for (int k = 1; k <= nz; ++k)
     for (int i = 1; i <= nx; ++i)
     {
-        const complex phi = kx*grid.x (k,i) + kz*grid.z (k,i) + 0.5*omega*dt;
+        const real phi = kx*grid.x (k,i) + kz*grid.z (k,i);
 
-        for (int j = 0; j < 3; ++j) A[j] (k,i) = std::real (Avec[j]*exp (I*phi));
+        for (int j = 0; j < 3; ++j) A[j] (k,i) = cos (phi)*std::real (Avec[j]);
+        for (int j = 0; j < 3; ++j) E[j] (k,i) = cos (phi)*std::real (Evec[j]);
     }
     boundCond (A);
-
-    for (int k = 1; k <= nz; ++k)
-    for (int i = 1; i <= nx; ++i)
-    {
-        const complex phi = kx*grid.x (k,i) + kz*grid.z (k,i);
-
-        for (int j = 0; j < 3; ++j) E[j] (k,i) = std::real (Evec[j]*exp (I*phi));
-    }
     boundCond (E);
-    
+
     global->B0.x = B0*sin (angle);
     global->B0.y = 0.0;
     global->B0.z = B0*cos (angle);
