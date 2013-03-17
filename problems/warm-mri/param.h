036344a6c70b0500a6993f5a36a160b5303b10b7
Tobias Heinemann <tobias.heinemann@gmail.com>
2013-03-16 20:25:14 -0700

    Added new target: warm-mri

---
+++ problems/warm-mri/param.h
@@ -0,0 +1,29 @@
+//
+//  param.h
+//  vf-pic
+//
+//  Created by Tobias Heinemann on 2/28/13.
+//  Copyright (c) 2013 Tobias Heinemann. All rights reserved.
+//
+
+#ifndef vf_pic_param_h
+#define vf_pic_param_h
+
+typedef double real;
+typedef unsigned int uint;
+
+namespace vfpic
+{
+    const uint nx = 64;
+    const uint nz = 256;
+
+    const uint npc = 256;
+
+    const uint nthreads = 16;
+
+    const uint alignment = 16;
+
+    const uint nghost = 2;
+}
+
+#endif
